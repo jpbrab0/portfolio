e285bdf60b46758f84861b325bd88b8c8494b102
João Pedro <jpresende502@gmail.com>
2020-10-08 14:54:29 -0300

    VAI MANO

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,18 +1,13 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./Pages/Home";
-import NewsLetter from "./Pages/NewsLetter";
 import Projects from "./Pages/Projects";
-import RemoveEmail from "./Pages/RemoveEmail";
-
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/projects" component={Projects} />
-        <Route path="/newsletter" component={NewsLetter} />
-        <Route path="/removemail" component={RemoveEmail} />
       </Switch>
     </BrowserRouter>
   );
