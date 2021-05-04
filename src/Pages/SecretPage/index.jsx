3ea16332823674abf00377ad74898c5db2781929
Joao Pedro <jpresende502@gmail.com>
2021-05-03 22:02:01 -0300

    Criando página secreta 👀

---
+++ src/Pages/SecretPage/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Secretpage = () => {
+  return(
+    <>
+      <h1>Parabéns você descobriu a página secreta!</h1>
+      <img src="https://w7.pngwing.com/pngs/1024/859/png-transparent-trollface-rage-comic-internet-troll-internet-meme-frustrated-troll-face-comics-white-face.png" alt="Troll face" width={500}/>
+    </>
+  )
+};
+export default Secretpage;
