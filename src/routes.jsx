3c453f58f874b153d723601b9208b66a45d9fa2a
Joao Pedro <jpresende502@gmail.com>
2021-05-03 22:02:13 -0300

    Criando nova rota

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Projects from "./Pages/Projects";
+import Secretpage from "./Pages/SecretPage";
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/projects" component={Projects} />
+        <Route path="/secretpage" component={Secretpage} />
       </Switch>
     </BrowserRouter>
   );
