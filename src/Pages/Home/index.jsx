0aec2990967234fc6a46c4c9c9347009c1a9b69f
Joao Pedro <jpresende502@gmail.com>
2021-06-19 10:07:19 -0300

    Mudando data de nascimento

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -25,10 +25,10 @@ const Home = () => {
                             <br />
                             I live in Campo Limpo Paulista,São Paulo I've been programming since I was 10 years old and my first learned programming language was Python 3
                             <br />
-                            Currently I'm 13 years old and web developer with this technologies: Javascript, Golang, Php, PostgreSQL and Node.js</p>
+                            Currently I'm 14 years old and web developer with this technologies: Javascript, Golang, Php, PostgreSQL, Node.js and Elixir</p>
                         </div>
                         <div className="img">
-                            <img src="https://github.com/jpbrab0.png" alt="Homem Programando" draggable={false}/>
+                            <img src="https://github.com/jpbrab0.png" draggable={false}/>
                         </div>
                     </section>
                 </section>
